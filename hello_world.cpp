f9d261824ef31003a151d7dacc06fc40100f21f9
ANUJ KUMAR <kumar.anuj.28082004@gmail.com>
2025-03-25 01:34:25 +0530

    first commit

---
+++ hello_world.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //headerfile
+using namespace std;
+int main(){ //start
+    cout<<"Hello World";
+    return 0; //exit
+}
---
+++ simple_interest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void main(){
+    int p;
+    int r;
+    int t;
+    float si;
+    cin>>p;
+    cin>>r;
+    cin>>t;
+    si=p*r*t/100;
+    cout<<si;
+    cout<<'\n';
+    return ;
+}
